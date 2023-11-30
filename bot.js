564896fba991ff9d89e4f1e6f0e54ca8a4f79ba5
Adrian Mrowka <mrowkaadrian@gmail.com>
2023-11-30 14:29:18 +0100

    Refactored (extracted duplicated code)

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
-const fs = require('node:fs');
-const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { config } = require('dotenv');
+const { getCommands } = require('./src/getCommands');
 
 config();
 
@@ -13,7 +12,7 @@ const client = new Client({
 });
 
 client.commands = new Collection();
-loadSlashCommands();
+getCommands(client);
 
 client.once('ready', () => {
     console.log('Bot is ready!');
@@ -31,22 +30,3 @@ client.on(Events.InteractionCreate, async interaction => {
 
 client.login(process.env.TOKEN);
 
-function loadSlashCommands() {
-    const foldersPath = path.join(__dirname, 'commands');
-    const commandFolders = fs.readdirSync(foldersPath);
-
-    for (const folder of commandFolders) {
-        const commandsPath = path.join(foldersPath, folder);
-        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-        for (const file of commandFiles) {
-            const filePath = path.join(commandsPath, file);
-            const command = require(filePath);
-            // Set a new item in the Collection with the key as the command name and the value as the exported module
-            if ('data' in command && 'execute' in command) {
-                client.commands.set(command.data.name, command);
-            } else {
-                console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-            }
-        }
-    }
-}
