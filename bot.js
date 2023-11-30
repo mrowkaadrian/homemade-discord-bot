581e0b2d8bce33992d21818696ceaef2494cb7e1
Adrian Mrowka <mrowkaadrian@gmail.com>
2023-11-30 01:24:31 +0100

    Cleanup

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
-const {Client, GatewayIntentBits} = require('discord.js');
-const ytdl = require('ytdl-core');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const config = require('./config');
 
 const client = new Client({
     intents: [
@@ -7,64 +9,13 @@ const client = new Client({
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
     ],
     disableMentions: 'everyone',
 });
-
-client.config = require('./config');
 
 client.once('ready', () => {
     console.log('Bot is ready!');
 });
 
-client.on('message', async (message) => {
-    if (message.content === 'ping')
-        message.reply('pong');
-
-    // Ignore messages from bots
-    if (message.author.bot) return;
-
-    // Check if the message starts with the command
-    if (message.content.startsWith('!play')) {
-        // Get the voice channel of the user who sent the message
-        const voiceChannel = message.member.voice.channel;
-
-        // Check if the user is in a voice channel
-        if (!voiceChannel) {
-            return message.channel.send('You need to be in a voice channel to use this command!');
-        }
-
-        // Get the YouTube link from the message content
-        const link = message.content.slice('!play'.length).trim();
-
-        // Validate the YouTube link
-        if (!ytdl.validateURL(link)) {
-            return message.channel.send('Invalid YouTube link!');
-        }
-
-        // Join the user's voice channel
-        const connection = await voiceChannel.join();
-
-        // Play the YouTube video
-        const stream = ytdl(link, { filter: 'audioonly' });
-        const dispatcher = connection.play(stream);
-
-        dispatcher.on('start', () => {
-            message.channel.send(`Now playing: ${link}`);
-        });
-
-        dispatcher.on('finish', () => {
-            voiceChannel.leave();
-        });
-
-        dispatcher.on('error', (err) => {
-            console.error(err);
-            message.channel.send('An error occurred while playing the song.');
-            voiceChannel.leave();
-        });
-    }
-});
-
-// Replace 'YOUR_BOT_TOKEN' with your actual bot token
-client.login(client.config.app.token);
+client.login(config.app.token);
