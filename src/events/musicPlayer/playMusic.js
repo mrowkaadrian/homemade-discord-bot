fc9641fdbadfc063db62cc70de4410cd74986842
Adrian Mrowka <mrowkaadrian@gmail.com>
2023-12-07 23:15:00 +0100

    Added a way of choosing song from a dropdown

--- src/events/musicPlayer/playMusic.js
+++ src/events/musicPlayer/playMusic.js
@@ -1,31 +1,35 @@
-import { useMainPlayer, useQueue } from 'discord-player';
+import { useMainPlayer } from 'discord-player';
 import myUwuifier from '../../util/uwuifier.js';
 import { playerMenu } from '../../interaction/menus/musicPlayerMenu.js';
 import logger from '../../logging/logger.js';
-import { EmbedBuilder } from 'discord.js';
+import { ActionRowBuilder, EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from 'discord.js';
 
 export async function playMusic(interaction) {
+	await interaction.deferReply();
+
+	const selection = await showTracksAndExpectSelection(interaction);
+	if (!selection) {
+		return interaction.editReply('You did not select a track in time.');
+	}
+
 	const player = useMainPlayer();
 	const channel = interaction.member.voice.channel;
-	const query = interaction.options.getString('query');
-	const resource = await player.search(query, {
+	const audioResource = await player.search(selection, {
 		requestedBy: interaction.user,
 	});
 
 	if (!channel) {
-		return interaction.reply(myUwuifier.uwuifySentence('You must be in a voice channel to use this command'));
+		return interaction.editReply(myUwuifier.uwuifySentence('You must be in a voice channel to use this command'));
 	}
 
-	if (!resource.hasTracks()) {
-		return interaction.reply(`No results found for \`${query}\``);
+	if (!audioResource.hasTracks()) {
+		return interaction.editReply('Something went wrong while searching for the track. Check logs for more details.');
 	}
-
-	await interaction.deferReply();
 
 	try {
-		await player.play(channel, resource);
+		await player.play(channel, audioResource);
 
-		const resourceJSON = resource.toJSON();
+		const resourceJSON = audioResource.toJSON();
 		const trackInfo = resourceJSON.tracks[0];
 
 		const nowPlayingEmbed = new EmbedBuilder()
@@ -37,14 +41,59 @@ export async function playMusic(interaction) {
 				{ name: 'Duration', value: trackInfo.duration, inline: true },
 			);
 
-		await interaction.followUp({
+		await interaction.editReply({
 			embeds: [ nowPlayingEmbed ],
 			components: [ playerMenu ],
 		});
-		logger.info(`Executed play-music command with query: ${query}`);
+		logger.info(`Executed play-music command using resource: ${trackInfo.url}`);
 	}
 	catch (error) {
-		await interaction.followUp('Something went wrong while playing the music. Check logs for more details.');
-		logger.error(`Error executing play-music command with query: ${query}` + error);
+		await interaction.editReply('Something went wrong while playing the music. Check logs for more details.');
+		logger.error(`Error executing play-music command using resource: ${audioResource.toJSON().tracks[0].url}` + error);
 	}
+}
+
+async function showTracksAndExpectSelection(interaction) {
+	const player = useMainPlayer();
+	const query = interaction.options.getString('query');
+	const resource = await player.search(query, {
+		requestedBy: interaction.user,
+	});
+
+	const resourceJSON = resource.toJSON();
+	const tracks = resourceJSON.tracks;
+
+	if (tracks.length === 1) {
+		return tracks[0].url;
+	}
+
+	const select = new StringSelectMenuBuilder()
+		.setCustomId('track-selection')
+		.setPlaceholder('Select a track to play')
+		.addOptions(
+			tracks.map(track => new StringSelectMenuOptionBuilder()
+				.setLabel(track.description).setValue(track.url)),
+		);
+
+	const row = new ActionRowBuilder().addComponents(select);
+
+	await interaction.reply({
+		components: [ row ],
+	});
+
+	let response;
+
+	try {
+		response = await interaction.channel.awaitMessageComponent({
+			filter: i => i.customId === 'track-selection',
+			time: 60000,
+		});
+	}
+	catch (e) {
+		logger.warn('User did not select a track in time');
+		return null;
+	}
+
+	// Returns the URL of the selected track
+	return response.values[0];
 }
